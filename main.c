a7df56831757a6428e1754c632c11825d6e6408b
PZQ0606 <1315881638@qq.com>
2026-03-04 21:12:13 +0800

    save print*

--- main.c
+++ main.c
@@ -1,16 +1,36 @@
 #include<stdio.h>
 
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+            
 void main()
 {
-    int i, j;
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
     
-    for(i = 1; i <= 5; i++) {
-        for(j = 1; j <= 5 - i; j++) {
-            printf(" ");
-        }
-        for(j = 1; j <= 2 * i - 1; j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
+    printf("Original array: ");
+    printArray(arr, n);
+    
+    bubbleSort(arr, n);
+    
+    printf("Sorted array: ");
+    printArray(arr, n);
 }
